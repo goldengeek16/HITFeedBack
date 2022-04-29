b9c11ee94f2c582c7e6d6f78fa28c29cbaae90fa
goldengeek16 <104579274+goldengeek16@users.noreply.github.com>
2022-04-29 15:59:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!--golden_geek-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>FeedbacK Engine</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elegant Feedback Form  Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+</head>
+<body class="agileits_w3layouts">
+    <h1 class="agile_head text-center">Harare Institute of Technology</h1>
+    <div class="w3layouts_main wrap">
+	  <h3>Please help us to serve you better by taking a couple of minutes. </h3>
+	    <form action="feedback.php" method="post" class="agile_form">
+		  <h2>To what degree is HIT as an University is an University providing good Services</h2>
+			 <ul class="agile_info_select">
+			 <li><input type="checkbox" name="#" value="excellent" id="excellent" required> 
+				 	  <label for="excellent">Campus</label>
+				      
+				 </li>
+				 <li><input type="checkbox" name="view" value="good" id="good"> 
+					  <label for="good">Location</label>
+				  
+				 </li>
+				 <li><input type="checkbox" name="view" value="neutral" id="neutral">
+					 <label for="neutral">Atmosphere</label>
+				    
+				 </li>
+				 <li><input type="checkbox" name="view" value="poor" id="poor"> 
+					  <label for="poor">Sports</label>
+				      
+				 </li>
+				 <li><input type="checkbox" name="view" value="poor" id="poor"> 
+					  <label for="poor">Students</label>
+				     
+				 </li>
+			<br>
+			<br>
+				 <li><input type="radio" name="view" value="excellent" id="excellent" required> 
+				 	  <label for="excellent">Friends</label>
+				      <div class="check w3"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="good" id="good"> 
+					  <label for="good"> Television</label>
+				      <div class="check w3ls"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="neutral" id="neutral">
+					 <label for="neutral">Internet</label>
+				     <div class="check wthree"></div>
+				 </li>
+				 <li><input type="radio" name="view" value="poor" id="poor"> 
+					  <label for="poor">Other</label>
+				      <div class="check w3_agileits"></div>
+				 </li>
+			 </ul>	  
+			<h2>If you have specific feedback, please write to us...</h2>
+			<textarea placeholder="Additional comments" class="w3l_summary" name="comments" required=""></textarea>
+			<input type="text" placeholder="Your Name (optional)" name="name"  />
+			<input type="email" placeholder="Your Email (optional)" name="email"/>
+			<input type="text" placeholder="Your Address (optional)" name="num"  /><br>
+			<center><input type="submit" value="submit Feedback" class="agileinfo" /></center>
+	  </form>
+	</div>
+	<div>
+	<center><p font-color="white">HIT Feedback : Golden F Mudonhi (H210708T) </p></center>
+	</div>
+</body>
+</html>
+
